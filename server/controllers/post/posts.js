9a7db59a412b907d644b1ea97686b759fce2dd9c
LEE JAE SHIN <eoyld1231@naver.com>
2022-05-24 00:04:52 +0900

    이미지보이게

--- server/controllers/post/posts.js
+++ server/controllers/post/posts.js
@@ -7,9 +7,14 @@ module.exports = {
       include: [{ model: db["user"] }],
     });
 
+    for (let i = 0; i < result.length; i++) {
+      delete result[i].dataValues.user.dataValues.password;
+    }
+
     const data = result.map((el) => {
       return el.dataValues;
     });
+
     res.status(200).json({ data, message: "ok" });
-  }
+  },
 };
