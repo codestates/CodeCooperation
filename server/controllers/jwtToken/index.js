6dc2528d24e2f6ef7dae177450d9a48cee59cf07
JKS5 <jokekhanson@gmail.com>
2022-05-25 18:48:18 +0900

    pull

--- server/controllers/jwtToken/index.js
+++ server/controllers/jwtToken/index.js
@@ -1,24 +1,26 @@
-require('dotenv').config();
-const { sign, verify } = require('jsonwebtoken');
+require("dotenv").config();
+const { sign, verify } = require("jsonwebtoken");
 
 module.exports = {
-    generateAccessToken : (data) => {
-        // console.log(data)
-        const accessToken = sign({ data }, process.env.ACCESS_SECRET, 
-            { algorithm: process.env.ALGORITHM, expiresIn: process.env.EXPIRESIN });
-        return accessToken;
-    },
+  generateAccessToken: (data) => {
+    // console.log(data)
+    const accessToken = sign({ data }, process.env.ACCESS_SECRET, {
+      algorithm: process.env.ALGORITHM,
+      expiresIn: process.env.EXPIRESIN,
+    });
+    return accessToken;
+  },
 
-    isAuthorized : (req) => {
+  isAuthorized: (req) => {
     const authorization = req.headers["authorization"];
     if (!authorization) {
       return null;
     }
-    const token = authorization.split(" ")[1];
+    const token = authorization;
     try {
       return verify(token, process.env.ACCESS_SECRET);
     } catch (err) {
       return null;
     }
   },
-}
+};
